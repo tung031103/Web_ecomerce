662a945a34eef096e2cdd38c2a83c6a8d451298e
TUng Hoang <hoangtung240603@gmail.com>
2022-11-23 21:36:42 +0700

    4 commit

--- main.js
+++ main.js
@@ -3,17 +3,16 @@ const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
 
-if(bar){
-    bar.addEventListener('click',()=>{
+if (bar) {
+    bar.addEventListener('click', () => {
         nav.classList.add('active');
     });
 
 }
 
-
-if(close){
-    close.addEventListener('click',()=>{
+if (close) {
+    close.addEventListener('click', () => {
         nav.classList.remove('active');
     });
-    
+
 }
